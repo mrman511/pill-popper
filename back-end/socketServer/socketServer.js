f319364b5226adb186acf9cd26c2d9addf89227f
Paul Bodner <bodner80@gmail.com>
2022-04-11 13:36:01 -0700

    created new Production branch

--- back-end/socketServer/socketServer.js
+++
@@ -1,9 +0,0 @@
-const socketio = require('socket.io');
-
-const listen = function(httpServer) {
-  const server = socketio(httpServer);
-  server.on('connection', () => { 'Client has connected to the socket server' });
-  return server;
-}
-
-module.exports = { listen };
